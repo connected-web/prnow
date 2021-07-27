36127e3c1b8be8ca6d071c31105968d900f5f4aa
John Beech <johnbeech.github@mkv25.net>
2021-07-27 08:24:17 +0100

    #14 Use dots instead of slashes as separator in branch paths See: https://github.com/connected-web/prnow/issues/14

--- src/steps/createBranch.js
+++ src/steps/createBranch.js
@@ -2,6 +2,9 @@ const exec = require('../util/asyncExec')
 const createBranchNameSlug = require('../util/createBranchNameSlug')
 const report = (...messages) => console.log('[PR Now] [Create Branch]', ...messages)
 
+const LEGACY_PATH_SEPARATOR = '/'
+const BRANCH_PATH_SEPARATOR = '.'
+
 async function createBranch (workingKnowledge) {
   const { ticket, ticketTitle, ticketUrl, cwd } = workingKnowledge
   // - Create a branch equivalent to the ticket name
@@ -9,14 +12,37 @@ async function createBranch (workingKnowledge) {
   const currentBranchName = (await exec('git branch', { cwd })).stdout.split('\n').filter(n => /\* /.test(n))[0].substr(2)
   report('Current Branch Name', currentBranchName)
 
-  let branchName = ticketTitle ? `${ticket}/${createBranchNameSlug(ticketTitle)}` : ticket
+  const titleSlug = createBranchNameSlug(ticketTitle)
+  const legacyBranchName = [ticket, titleSlug].join(LEGACY_PATH_SEPARATOR)
+  const extendedBranchName = [ticket, titleSlug].join(BRANCH_PATH_SEPARATOR)
+  const branchName = ticketTitle ? extendedBranchName : ticket
 
-  if (currentBranchName === branchName) {
-    report(`Already on branch ${currentBranchName}`)
-  } else if (branchName.split('/')[0] === currentBranchName.split('/')[0]) {
-    branchName = currentBranchName
+  const checkForExactMatch = branchName
+  const checkForLegacyTicket = legacyBranchName
+  const checkForExtendedTicket = extendedBranchName
+
+  const outcomes = {}
+  outcomes[checkForExactMatch] = alreadyOnBranch
+  outcomes[checkForLegacyTicket] = switchToNewBranchFormat
+  outcomes[checkForExtendedTicket] = reusingExistingBranch
+  outcomes.default = checkoutNewBranch
+
+  const action = outcomes[currentBranchName] || outcomes.default
+  await action({ branchName, report })
+
+  async function alreadyOnBranch ({ branchName, report }) {
+    report(`Already on branch ${branchName}`)
+  }
+
+  async function switchToNewBranchFormat ({ branchName, report }) {
+    report('Switching to new branch format:', branchName)
+  }
+
+  async function reusingExistingBranch ({ branchName, report }) {
     report('Reusing existing branch:', branchName)
-  } else {
+  }
+
+  async function checkoutNewBranch ({ branchName, report }) {
     const { stdout, stderr } = await exec(`git checkout -b "${branchName}"`, { cwd })
     report('git checkout:', stdout, stderr)
   }
